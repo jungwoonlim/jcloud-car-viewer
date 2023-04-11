3e27781f2eb548586f7ec31949f66f668595e6e3
jwlim <jwlim01@kcp.co.kr>
2023-04-11 12:48:15 +0900

    refactor: 기본 설정을 react에서 vue3로 변경

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <div class="title">
+      jCloud Car Viewer
+    </div>
+    <div class="sub-description">
+      간단한 3D 자동차 모델 뷰어
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style lang="scss" scoped>
+.app {
+  .title {
+    color: blue;
+  }
+
+  .sub-description {
+    color: skyblue;
+  }
+}
+</style>
