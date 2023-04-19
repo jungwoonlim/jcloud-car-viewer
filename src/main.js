aa8eeccdef1f1190da0923b4268a8f3142105a68
jCloud <jwlim102@gmail.com>
2023-04-19 12:47:29 +0900

    feat: gltf loader 추가 및 3D 모델 뷰어 추가

--- src/main.js
+++ src/main.js
@@ -1,32 +1,77 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-/** Renderer */
-const canvas = document.querySelector("#renderer");
+/** Canvas */
+const canvas = document.querySelector('#renderer');
+
+/** Scene */
 const scene = new THREE.Scene();
 
-const mesh = new THREE.Mesh(
-  new THREE.TorusGeometry(1, 0.4, 16, 60),
-  new THREE.MeshToonMaterial({
-    color: "#2f2f2f",
-  })
+/** Camera */
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.25,
+  100,
 );
-scene.add(mesh);
+camera.position.set(0, 0.25, 0);
 
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
-directionalLight.position.set(1, 1, 0)
-scene.add(directionalLight)
-
-const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.z = 6
-scene.add(camera)
-
+/** Renderer */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  alpha: true
-})
+  canvas,
+  antialias: true,
+});
+renderer.setClearColor(0xf2f2f2);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+/** Light */
+const color = 0xffffff;
+const intensity = 1;
+
+/** Hemisphere Light */
+const hemiLight = new THREE.HemisphereLight(color, color, intensity);
+scene.add(hemiLight);
+
+/** Directional Light */
+const firstDirectLight = new THREE.DirectionalLight(color, intensity);
+firstDirectLight.position.set(15, 100, -20);
+scene.add(firstDirectLight);
+
+const secondDirectLight = new THREE.DirectionalLight(color, intensity);
+secondDirectLight.position.set(-55, 50, -20);
+scene.add(secondDirectLight);
+
+/** Model Loader */
+const loader = new GLTFLoader();
+loader.load("scene.gltf", (gltf) => {
+  const { scene: model } = gltf;
+  console.log(model)
+
+  model.traverse((info) => {
+    if (!info.isMesh) return;
+    info.castShadow = true;
+  });
+
+  // model.scale.set(50, 50, 50);
+  model.scale.set(2, 2, 2);
+  model.position.set(0, 0, -0.5);
+  scene.add(model);
+
+  render();
+});
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+});
 
 const render = () => {
-  renderer.render(scene, camera)
-}
-
-render();
+  window.requestAnimationFrame(render);
+  renderer.render(scene, camera);
+};
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 export default {
   root: "src/",
+  publicDir: '../static/',
   base: "./",
   build: {
     outDir: '../dist',
