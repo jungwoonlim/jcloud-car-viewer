a7b5a96b52d2230b52e6639b73c9dfbc51657f49
jCloud <jwlim102@gmail.com>
2023-04-27 23:27:07 +0900

    feat: 빛 에니메이션 효과 추가 작업

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 npx create-react-app . --template minimal
 npm i -D three @react-three/fiber @react-three/drei @react-three/postprocessing
 ```
+
+### gltf 다운로드 링크
+
+[Mclaren MP4-12C Ultimate](https://sketchfab.com/3d-models/mclaren-mp4-12c-ultimate-80f19f7f5aac428985356987769538fe)
--- src/components/Light.js
+++ src/components/Light.js
@@ -1,5 +1,24 @@
+import { useFrame } from "@react-three/fiber";
+import { useState } from "react";
+
 export const Ligth = () => {
-  const position = [5, 5, 0];
+  const [position, setPosition] = useState([5, 5, 0]);
+  const [speed, setSpeed] = useState(0.1);
+
+  useFrame(() => {
+    if (speed === 0.1 && position[0] > 15) {
+      setSpeed(-0.1);
+    }
+    if (speed === -0.1 && position[0] < -15) {
+      setSpeed(0.1);
+    }
+
+    setPosition([
+      position[0] + speed,
+      position[1],
+      position[2],
+    ]);
+  });
 
   return (
     <>
@@ -17,7 +36,15 @@ export const Ligth = () => {
         intensity={0.3}
         angle={0.9}
         penumbra={0.1}
-        position={position}
+        position={[-20, 3, 0]}
+        castShadow
+      />
+      <spotLight
+        color={[1, 1, 1]}
+        intensity={0.1}
+        angle={1}
+        penumbra={0.1}
+        position={[100, 1, 0]}
         castShadow
       />
     </>
