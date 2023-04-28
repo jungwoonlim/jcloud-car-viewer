72bc19bbb1a65ff34742739338ee0a1c15229edd
jCloud <jwlim102@gmail.com>
2023-04-28 13:24:31 +0900

    fix: renderer zoom 기능 막기

--- src/components/renderer/Renderer.js
+++ src/components/renderer/Renderer.js
@@ -6,7 +6,7 @@ import { Ligth } from "./Light";
 export const Renderer = () => {
   return (
     <>
-      <OrbitControls target={[0, 1, 0]} maxPolarAngle={1.45} />
+      <OrbitControls target={[0, 1, 0]} maxPolarAngle={1.45} enableZoom={false} />
       <PerspectiveCamera makeDefault fov={50} position={[4, 2, 4]} />
       <color args={[0, 0, 0]} attach="background" />
       <Ligth />
