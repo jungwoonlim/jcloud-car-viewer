8774a28af8c91260cc4b9c96744a9c0b48e5a36f
jCloud <jwlim102@gmail.com>
2023-04-29 20:12:27 +0900

    chore: 빌드툴 변경 (cra -> vite)

---
+++ src/components/container/MainContainer.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export const MainContainer = () => {
+  const [showModel, setShowModel] = useState(false);
+
+  if (showModel) {
+    return (
+      <div className="back-container">
+        <div className="title">
+          Mclaren MP4-12C Ultimate
+        </div>
+        <div className="button color2" onClick={() => setShowModel(false)}>
+          Show Description
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="main-container">
+      <div className="card">
+        <div className="title">
+          Mclaren MP4-12C Ultimate
+        </div>
+        <div className="description">
+          <p>
+            The McLaren MP4-12C, later known simply as the McLaren 12C, is a sports car that was designed and manufactured by McLaren Automotive. It was the first ever production car wholly designed and built by McLaren, and their first production road car since the McLaren F1, which was last built in 1998. The car's final design was unveiled in September 2009 and was launched in mid-2011.
+          </p>
+          <p>
+            The MP4-12C uses a carbon fibre composite chassis, and is powered by a longitudinally-mounted McLaren M838T 3.8 L (3,799 cc) twin-turbocharged V8 engine, generating approximately 600 PS (592 hp; 441 kW) at 7,500 rpm and 600 N⋅m (443 lbf⋅ft) of torque at 5,600 rpm. The car makes use of Formula 1-sourced technologies such as "brake steer", where the inside rear wheel is braking during fast cornering to reduce understeer. Power is transmitted to the wheels through a seven-speed dual-clutch transmission Seamless-Shift gearbox (SSG) manufactured by Graziano Trasmissioni.
+          </p>
+          <p>
+            A convertible version of the car called the MP4-12C Spider, renamed the 12C Spider in 2012, was also available. In February 2014, McLaren announced the related 650S, with revised bodywork, upgraded engine and other technical improvements. In April 2014, McLaren announced the end of production of the 12C.
+          </p>
+        </div>
+        <div className="button color1" onClick={() => setShowModel(true)}>
+          Show Model
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/renderer/Ground.jsx
@@ -0,0 +1,56 @@
+import { MeshReflectorMaterial } from "@react-three/drei";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
+
+export const Ground = () => {
+  const [roughness, normal] = useLoader(TextureLoader, [
+    '/textures/terrain-roughness.jpg',
+    '/textures/terrain-normal.jpg',
+  ]);
+
+  useEffect(() => {
+    [normal, roughness].forEach((texture) => {
+      texture.wrapS = RepeatWrapping;
+      texture.wrapT = RepeatWrapping;
+      texture.repeat.set(5, 5);
+    });
+
+    normal.encoding = LinearEncoding;
+  }, [normal, roughness]);
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const goTime = - time * 0.128;
+
+    roughness.offset.set(0, goTime % 1);
+    normal.offset.set(0, goTime % 1);
+  });
+
+  return (
+    <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
+      <planeGeometry args={[30, 30]} />
+      <MeshReflectorMaterial
+        envMapIntensity={0}
+        normalMap={normal}
+        normalScale={[0.15, 0.15]}
+        roughnessMap={roughness}
+        dithering={true}
+        color={[0.015, 0.015, 0.015]}
+        roughness={0.7}
+        blur={[1000, 400]}
+        mixBlur={30}
+        mixStrength={80}
+        mixContrast={1}
+        resolution={1024}
+        mirror={0}
+        depthScale={0.01}
+        minDepthThreshold={0.9}
+        maxDepthThreshold={1}
+        depthToBlurRatioBias={0.25}
+        debug={0}
+        reflectorOffset={0.2}
+      />
+    </mesh>
+  );
+};
