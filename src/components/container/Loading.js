94a644e191bd151c5599efcb55c53b42363546c4
jCloud <jwlim102@gmail.com>
2023-04-29 11:50:31 +0900

    feat: 로딩 프로그레스 퍼센트 추가

--- src/components/container/Loading.js
+++ src/components/container/Loading.js
@@ -1,4 +1,5 @@
 import { useLottie } from 'lottie-react';
+import { useProgress } from '@react-three/drei';
 import animationData from '../../assets/loading.json'
 
 export const Loading = () => {
@@ -8,10 +9,13 @@ export const Loading = () => {
   };
 
   const { View } = useLottie(defaultOptions);
+  const { progress } = useProgress();
 
   return (
     <div className='loading'>
       { View }
+      <p>자동차 정보를 불러오고 있어요. {Math.floor(progress)}%</p>
+      <span>처음 불러올 때는 시간이 좀 걸릴 수 있어요.</span>
     </div>
   );
 };
